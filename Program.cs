460309ca41a5294af734507d957095024834cbdc
DragosPopescu2308 <dragospopescu74@yahoo.com>
2025-03-26 22:16:02 +0200

    Varianta initiala - cod procedural, fara OOP

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] Animals = { "Dog", "Cat", "Cow" };
+        string[] Sounds = { "Ham! Ham!", "Miau!", "MUUUU!" };
+
+        for(int i=0; i<Animals.Length; i++)
+        {
+            Console.WriteLine($"{Animals[i]} makes sound:  {Sounds[i]}");
+        }
+    }
+}
